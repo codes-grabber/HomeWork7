f4fba564e357c5b997cd3fe51740ea78b96a0fd0
WORK <codes-grabber@yandex.ru>
2021-11-14 15:34:18 +0400

    Графические интерфейсы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        new MyWindow();
+//        new MyWindow();
+
+        String[] words = {"apple",
+                "orange",
+                "lemon",
+                "banana",
+                "apricot",
+                "avocado",
+                "broccoli",
+                "carrot",
+                "cherry",
+                "garlic",
+                "grape",
+                "melon",
+                "leak",
+                "kiwi",
+                "mango",
+                "mushroom",
+                "nut",
+                "olive",
+                "pea",
+                "peanut",
+                "pear",
+                "pepper",
+                "pineapple",
+                "pumpkin",
+                "potato"};
+
+        WordTest firstWord = new WordTest("Gay");
+
+
     }
 }
---
+++ src/WordTest.java
@@ -0,0 +1,15 @@
+public class WordTest {
+    String word;
+    String userWord;
+
+    WordTest(String userWord){
+        this.userWord = userWord;
+        System.out.println(userWord);
+
+    }
+
+//    void seeWord (int a){
+//        int a = (int) (Math.random()*3);
+//        System.out.println();
+//    }
+}
